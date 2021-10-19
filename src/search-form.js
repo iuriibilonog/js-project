2431513cd2232aa7d39305c8cbf930441315da83
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-10-19 17:51:20 +0300

    done?

--- src/search-form.js
+++ src/search-form.js
@@ -1,9 +1,37 @@
 import { fetchPictures } from './fetchData';
 
+import countries from './countries.json'
 
 
-const getCountryList = fetchPictures().then(data => {
+const searchForm = document.querySelector('#search-form');
 
-});
 
-console.log(getCountryList)
+function printOptions(data) {
+    const optionList = document.querySelector('#choose-country');
+    const markUp = data
+        .map(item => {
+            const { countryCode, countryName } = item;
+            return `<option data-code=${countryCode}>${countryName}</option>`;
+        }).join('');
+    optionList.insertAdjacentHTML('beforeend', markUp);
+}
+
+printOptions(countries);
+
+
+const searchInput = document.querySelector('#search-input');
+const searchCountry = document.querySelector('#search-country');
+
+
+searchForm.addEventListener('input', getData)
+
+
+function getData() {
+    fetchPictures(searchInput.value, searchCountry.value)
+    console.log(searchInput.value)
+    console.log(searchCountry.value)
+    console.error('---')
+};
+
+
+
