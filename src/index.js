aed9b446e97d8abcf1a8c11789e885e8119fcfa1
DValeriy <valeriydm20@gmail.com>
2021-10-20 13:58:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,5 +12,15 @@ window.onload = async function () {
   const data = await getDataServer();
   console.log(data);
   await showData(data._embedded.events);
+  (() => {
+    if ('loading' in HTMLImageElement.prototype) {
+      const lazyLoadImg = document.querySelectorAll('.lazyload');
+      lazyLoadImg.forEach(img => (img.src = img.dataset.src));
+      // <img src="image.jpg" loading="lazy" alt="..." />;
+    } else {
+      require('lazysizes');
+      // <img data-src="image.jpg" class="lazyload" />;
+    }
+  })();
   modalWindow();
 };
