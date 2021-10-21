5c4cf70c9abc396aae758488517682e8de622812
Iurii Bilonog <iuriibilonog@gmail.com>
2021-10-21 21:55:36 +0300

    change search-form and autofill

--- src/search-form.js
+++ src/search-form.js
@@ -1,7 +1,5 @@
 import { getDataServer } from './js/fetchData';
 import { debounce } from 'debounce';
-import { showData } from './js/showData';
-import { showPagination } from './js/pagination';
 import { showPreloader } from './js/preload';
 
 import countries from './countries.json';
@@ -19,7 +17,7 @@ function printOptions(data) {
 
 printOptions(countries);
 
-document.querySelector('#search-form').addEventListener('input', debounce(getCards, 1000));
+document.querySelector('#search-form').addEventListener('input', debounce(getCards, 500));
 
 function getCards() {
   const searchInput = document.querySelector('#search-input');
@@ -27,43 +25,18 @@ function getCards() {
   const searchCountryOption = document.querySelector(
     `#choose-country option[value="${searchCountry.value}"]`,
   );
-  // console.log(searchCountryOption.textContent)
-
-  // getDataServer(searchInput.value);
-
-  // const countryCodeCheck = countries.some(
-  //   item => item.countryCode === searchCountryOption?.textContent,
-
-  // );
-  // console.log(searchCountryOption.textContent)
-
-  // if (countryCodeCheck || !searchCountryOption.textContent) {
-  //   console.log(countryCodeCheck)
-  //   getDataServer(searchInput.value, searchCountryOption.textContent);
-  // }
   const keywordValidator = !searchInput.value ? '' : searchInput.value;
   const countryValidator = !searchCountryOption?.textContent
     ? ''
     : searchCountryOption?.textContent;
 
+  localStorage.setItem('searchData', searchInput.value);
   localStorage.setItem('localCountryName', searchCountry.value);
   localStorage.setItem('localCountryCode', searchCountryOption.textContent);
 
   const promisePreload = showPreloader();
 
   getDataServer(keywordValidator, countryValidator)
-    .then(data => {
-      /*       if (data._embedded) {
-        document.querySelector('.events__list').innerHTML = '';
-        showData(data._embedded.events);
-        showPagination(
-          1,
-          +data.page.number + 1,
-          +data.page.totalPages >= 50 ? 49 : +data.page.totalPages,
-        );
-        console.log('11111');
-      }
- */ return promisePreload;
-    })
+    .then(() => promisePreload)
     .then(preloadNode => preloadNode.remove());
 }
