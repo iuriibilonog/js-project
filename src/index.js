d26f6f5ee7dcc6bc03255fc18307084f2edb5e79
Artowod <myjavaname@gmail.com>
2021-10-21 18:00:25 +0300

    structurizing-1

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import './autofil-search-form';
 import './search-form';
 import './preload';
 import { getDataServer, firstQueryDataServer } from './fetchData';
-import './events';
-
 import './modal-window';
-
 //add scroll to top button
 import './scrollUp';
